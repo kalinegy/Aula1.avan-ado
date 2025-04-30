89bb0532baad68ce7dc884f482fab83e2c8b0c6d
kaline gabrielly <gkaline769@gmail.com>
2025-04-30 15:10:16 -0300

    exerc3(revisar)

--- Biblioteca.py
+++ Biblioteca.py
@@ -6,3 +6,10 @@ def piramide(num):
         for y in range(0, x):
             print(x, end=" ")
         print()
+
+def Contavogais(texto):
+    cont=0
+    for x in range(len(texto)):
+        if texto[x]== "a" or texto[x]== "e" or texto[x]== "i" or texto[x]== "o" or texto[x]== "u" or texto[x]== "A" or texto[x]== "E" or texto[x]== "I" or texto[x]== "O" or texto[x]== "U":
+            cont=cont+1
+        print(cont)
---
+++ exerc3.py
@@ -0,0 +1,4 @@
+from Biblioteca import Contavogais
+texto="O rato roeu a roupa do rei de roma"
+Contavogais(texto)
+
